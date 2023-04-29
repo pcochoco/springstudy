40864f457230d61273cc4f514c17b0ebe92de63b
Bin0917 <leesd132@naver.com>
2023-04-29 12:15:06 +0900

    스프링 스터디

---
+++ hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
@@ -0,0 +1,17 @@
+package hello.hellospring.service;
+
+import hello.hellospring.repository.MemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class SpringConfig {
+    private final MemberRepository memberRepository;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+
+}
